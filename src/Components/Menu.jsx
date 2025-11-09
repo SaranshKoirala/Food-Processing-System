68218ef813aea1830fd1006206384d80e1b5e179
Saransh Koirala <upwhats498@gmail.com>
2025-11-09 22:59:18 +0545

    description page added

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,38 +1,37 @@
 import { useEffect, useState } from 'react';
 import { IoIosSearch } from 'react-icons/io';
-import { CiShoppingCart } from 'react-icons/ci';
-import { MdOutlineDelete } from 'react-icons/md';
-import { FiMinus } from 'react-icons/fi';
-import { FiPlus } from 'react-icons/fi';
 
 import FoodCard from './FoodCard';
+import axios from 'axios';
+import Navbar from './Navbar';
 
 export default function Menu() {
   const [activeId, setActiveId] = useState(0);
-  const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItem, setCartItem] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   function handleMenuCategory(id) {
     setActiveId(id);
   }
 
-  function toggleCart() {
-    setIsCartOpen((isOpen) => !isOpen);
-  }
-
   function handleCartItem(item) {
-    setCartItem((cartItem) => [...cartItem, item]);
-  }
+    setCartItem((cartItem) => {
+      const existingItemIndex = cartItem.findIndex(
+        (prevProduct) => prevProduct.id === item.id
+      );
 
-  useEffect(() => {
-    if (isCartOpen) {
-      document.documentElement.classList.add('overflow-hidden');
-      document.body.classList.add('overflow-hidden');
-    } else {
-      document.documentElement.classList.remove('overflow-hidden');
-      document.body.classList.remove('overflow-hidden');
-    }
-  }, [isCartOpen]);
+      if (existingItemIndex !== -1) {
+        // Product exists - increase quantity
+        const updatedCart = [...cartItem];
+        updatedCart[existingItemIndex].quantity += 1;
+        return updatedCart;
+      } else {
+        // New product - add with quantity 1
+        return [...cartItem, { ...item, quantity: 1 }];
+      }
+    });
+  }
 
   const menuCategory = [
     {
@@ -57,254 +56,26 @@ export default function Menu() {
     },
   ];
 
-  const foodDetails = [
-    {
-      id: 1,
-      name: 'Pizza',
-      image: '',
-      price: 300,
-      category: 'pizza',
-      description:
-        'This is the italian pizza replicated with our own magic and perfection.',
-      veg: false,
-    },
-    {
-      id: 2,
-      name: 'Pizza',
-      image: '',
-      price: 300,
-      category: 'pizza',
-      description:
-        'This is the italian pizza replicated with our own magic and perfection.',
-      veg: false,
-    },
-    {
-      id: 3,
-      name: 'Pizza',
-      image: '',
-      price: 300,
-      category: 'pizza',
-      description:
-        'This is the italian pizza replicated with our own magic and perfection.',
-      veg: false,
-    },
-    {
-      id: 15,
-      name: 'Pizza',
-      image: '',
-      price: 300,
-      category: 'pizza',
-      description:
-        'This is the italian pizza replicated with our own magic and perfection.',
-      veg: false,
-    },
-    {
-      id: 4,
-      name: 'Pizza',
-      image: '',
-      price: 300,
-      category: 'pizza',
-      description:
-        'This is the italian pizza replicated with our own magic and perfection.',
-      veg: false,
-    },
-    {
-      id: 5,
-      name: 'Pizza',
-      image: '',
-      price: 300,
-      category: 'pizza',
-      description:
-        'This is the italian pizza replicated with our own magic and perfection.',
-      veg: false,
-    },
-    {
-      id: 6,
-      name: 'Pizza',
-      image: '',
-      price: 300,
-      category: 'pizza',
-      description:
-        'This is the italian pizza replicated with our own magic and perfection.',
-      veg: false,
-    },
-    {
-      id: 7,
-      name: 'Pizza',
-      image: '',
-      price: 300,
-      category: 'pizza',
-      description:
-        'This is the italian pizza replicated with our own magic and perfection.',
-      veg: false,
-    },
-    {
-      id: 8,
-      name: 'Pizza',
-      image: '',
-      price: 300,
-      category: 'pizza',
-      description:
-        'This is the italian pizza replicated with our own magic and perfection.',
-      veg: false,
-    },
-    {
-      id: 9,
-      name: 'Pizza',
-      image: '',
-      price: 300,
-      category: 'pizza',
-      description:
-        'This is the italian pizza replicated with our own magic and perfection.',
-      veg: false,
-    },
-    {
-      id: 10,
-      name: 'Pizza',
-      image: '',
-      price: 300,
-      category: 'pizza',
-      description:
-        'This is the italian pizza replicated with our own magic and perfection.',
-      veg: false,
-    },
-    {
-      id: 11,
-      name: 'Pizza',
-      image: '',
-      price: 300,
-      category: 'pizza',
-      description:
-        'This is the italian pizza replicated with our own magic and perfection.',
-      veg: false,
-    },
-    {
-      id: 12,
-      name: 'Pizza',
-      image: '',
-      price: 300,
-      category: 'pizza',
-      description:
-        'This is the italian pizza replicated with our own magic and perfection.',
-      veg: false,
-    },
-    {
-      id: 13,
-      name: 'Pizza',
-      image: '',
-      price: 300,
-      category: 'pizza',
-      description:
-        'This is the italian pizza replicated with our own magic and perfection.',
-      veg: false,
-    },
-    {
-      id: 14,
-      name: 'Pizza',
-      image: '',
-      price: 300,
-      category: 'pizza',
-      description:
-        'This is the italian pizza replicated with our own magic and perfection.',
-      veg: false,
-    },
-  ];
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        setLoading(true);
+        const res = await axios.get('http://127.0.0.1:8000/api/products');
+        const data = res.data;
+        setProducts(data);
+      } catch (err) {
+        console.error('Failed to fetch the products!', err);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchProducts();
+  }, []);
+
   return (
     <div>
-      <nav className='flex justify-between items-center bg-card/80 shadow-[0_4px_6px_-2px_rgba(0,0,0,0.25)] mb-15 px-7 py-5'>
-        <h1 className='font-semibold text-amber-600 text-3xl'>
-          Delicious Menu
-        </h1>
-
-        {/* Cart */}
-        <div
-          className='group relative flex justify-center items-center hover:bg-amber-600 border-2 border-amber-600 rounded-full w-10 h-10 cursor-pointer'
-          onClick={toggleCart}>
-          <CiShoppingCart className='font-semibold group-hover:text-white text-2xl' />
-          {cartItem.length > 0 ? (
-            <p className='-top-3 -right-1 absolute flex justify-center items-center bg-amber-500 rounded-full w-5 h-5 text-white text-sm'>
-              {cartItem.length}
-            </p>
-          ) : (
-            <></>
-          )}
-        </div>
-      </nav>
-
-      {/* Sidebar */}
-      <div
-        className={`fixed top-0 right-0 h-full w-[450px] bg-white shadow-xl transition-transform duration-300 z-50 flex flex-col 
-        ${isCartOpen ? 'translate-x-0' : 'translate-x-full'}`}>
-        <div className='relative p-4'>
-          <h2 className='font-semibold text-xl'>Your Order</h2>
-          <button
-            onClick={toggleCart}
-            className='-top-1 right-4 absolute p-1 text-2xl cursor-pointer'>
-            ×
-          </button>
-        </div>
-
-        {/* Cart Items Here */}
-        {cartItem.length < 1 ? (
-          <div className='flex flex-col justify-center items-center p-3 h-[80%] text-gray-400'>
-            <CiShoppingCart className='font-extrabold text-6xl' />
-            <p className='text-sm'>Your cart is empty</p>
-          </div>
-        ) : (
-          <div className='flex flex-col items-center gap-4 p-4 h-[900px] overflow-y-scroll'>
-            {cartItem?.map((item) => (
-              <div className='relative flex justify-start items-center gap-5 p-3 border-black/20 last:border-0 border-b w-full'>
-                <img
-                  src='/burger.jpg'
-                  alt={item.name}
-                  className='rounded-xl w-20 h-17'
-                />
-                <div className='flex flex-col gap-1.5'>
-                  <p className='font-semibold text-[13px]'>{item.name}</p>
-                  <p className='font-semibold text-[13px] text-amber-500'>
-                    Rs {item.price}
-                  </p>
-
-                  <div className='flex justify-center items-center gap-2 rounded-xl'>
-                    <button className='hover:bg-amber-500 px-0.5 py-0.5 border border-black/30 hover:border-amber-500/50 rounded-md hover:text-white cursor-pointer'>
-                      <FiMinus className='text-[13px]' />
-                    </button>
-                    <p className='text-[13px]'>1</p>
-                    <button className='hover:bg-amber-500 px-0.5 py-0.5 border border-black/30 hover:border-amber-500/50 rounded-md hover:text-white cursor-pointer'>
-                      <FiPlus className='text-[13px]' />
-                    </button>
-                  </div>
-                </div>
-                <button className='top-16 right-8 absolute'>
-                  <MdOutlineDelete className='font-light text-amber-500' />
-                </button>
-              </div>
-            ))}
-          </div>
-        )}
-
-        {cartItem.length > 0 && (
-          <div className='px-6 pb-6'>
-            <div className='flex justify-between items-center pt-2 border-black/20 border-t text-black/40 text-sm'>
-              <p>Subtotal</p>
-              <p>200</p>
-            </div>
-            <div className='flex justify-between items-center mb-2 font-semibold'>
-              <p>Total</p>
-              <p>200</p>
-            </div>
-            <button className='bg-amber-500 p-2 rounded-xl w-full font-semibold text-white'>
-              Place Order
-            </button>
-          </div>
-        )}
-      </div>
-
-      {isCartOpen && (
-        <div
-          onClick={toggleCart}
-          className='z-40 fixed inset-0 backdrop-blur-sm brightness-50 overflow-hidden'></div>
-      )}
+      <Navbar cartItem={cartItem} setCartItem={setCartItem} />
 
       <div className='relative flex flex-col justify-center items-center gap-18'>
         <IoIosSearch className='top-4 left-95 absolute text-2xl' />
@@ -327,15 +98,21 @@ export default function Menu() {
             </li>
           ))}
         </ul>
-        <div className='gap-6 grid grid-cols-3 grid-flow-row'>
-          {foodDetails.map((item) => (
-            <FoodCard
-              details={item}
-              key={item.id}
-              handleCartItem={handleCartItem}
-            />
-          ))}
-        </div>
+        {loading ? (
+          <div className='flex justify-center items-center h-full'>
+            Loading...
+          </div>
+        ) : (
+          <div className='gap-6 grid grid-cols-3 grid-flow-row'>
+            {products?.map((item) => (
+              <FoodCard
+                details={item}
+                key={item.id}
+                handleCartItem={handleCartItem}
+              />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
--- src/pages/menu/FoodDescription.jsx
+++ src/pages/menu/FoodDescription.jsx
@@ -1,3 +1,125 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Navbar from '../../Components/Navbar';
+
 export default function FoodDescription() {
-  return <div>this is the food description</div>;
+  const { id } = useParams();
+
+  const [productDetail, setProductDetail] = useState();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchProduct() {
+      try {
+        setLoading(true);
+        const res = await axios.get(`http://127.0.0.1:8000/api/products/${id}`);
+        const data = res.data;
+        setProductDetail(data);
+        console.log(data);
+      } catch (err) {
+        console.error('Failed to fetch the products!', err);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchProduct();
+  }, []);
+
+  const recommendedProduct = [
+    {
+      id: 1,
+      name: 'Coca Cola',
+      price: 80,
+      image: '',
+    },
+    {
+      id: 1,
+      name: 'Coca Cola',
+      price: 80,
+      image: '',
+    },
+    {
+      id: 1,
+      name: 'Coca Cola',
+      price: 80,
+      image: '',
+    },
+    {
+      id: 1,
+      name: 'Coca Cola',
+      price: 80,
+      image: '',
+    },
+  ];
+
+  return (
+    <div>
+      <Navbar />
+      <div className='flex flex-col justify-start items-center gap-20 mx-auto p-5 w-fit'>
+        <div className='relative flex justify-center items-center gap-10'>
+          <div className='rounded-3xl w-[500px] h-[450px] overflow-hidden'>
+            <img
+              src='/burger.jpg'
+              alt={productDetail?.name}
+              className='rounded-2xl w-full h-full hover:rotate-2 hover:scale-110 transition-transform duration-700'
+            />
+          </div>
+          <div className='flex flex-col justify-between gap-3 h-[450px]'>
+            <div>
+              <h1 className='mb-2 font-bold text-5xl'>Margherita Pizza</h1>
+              <p className='text-amber-500 text-xl'>
+                Rs {productDetail?.price}
+              </p>
+            </div>
+            <div>
+              <p className='mb-1 font-semibold text-xl'>🍲 About This Dish</p>
+              <p className='w-92 font-extralight text-black/40 text-sm'>
+                {productDetail.description} This is one of the delicious food
+                you'll ever have. . Nothing beats the taste of this food. It is
+                made with specialist so don't fear , your money won't go to
+                waste.
+              </p>
+            </div>
+            <div className='flex flex-col justify-center gap-2'>
+              <p className='mb-1 font-semibold text-xl'>What's Included</p>
+              <ul className='grid grid-cols-2 text-sm list-disc list-inside'>
+                <li>Fresh Ingredients</li>
+                <li>Chief Special</li>
+                <li>Made to Order</li>
+                <li>Premium Quality</li>
+              </ul>
+            </div>
+            <button className='bg-amber-500 p-2 rounded-2xl cursor-pointer'>
+              Add To Cart
+            </button>
+          </div>
+          <div className='top-4 left-4 absolute bg-white px-2 py-1 rounded-md font-medium text-[13px]'>
+            Mains
+          </div>
+        </div>
+        <div className='self-start'>
+          <p className='mb-3 text-xl'>You may also like </p>
+          <ul className='flex justify-center items-center gap-4 list-none'>
+            {recommendedProduct.map((product) => (
+              <li className='shadow-2xl rounded-sm h-53'>
+                <img
+                  src='/burger.jpg'
+                  alt={product.name}
+                  className='rounded-t-md w-50 h-40'
+                />
+                <div className='px-2'>
+                  <p className='font-md text-sm'>{product.name}</p>
+                  <p className='font-md text-[13px] text-amber-600'>
+                    Rs. {product.price}
+                  </p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
