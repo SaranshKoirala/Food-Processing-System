847d8efcb6efc265809e0cc24c631b6e406279b9
Saransh Koirala <upwhats498@gmail.com>
2025-11-09 17:13:47 +0545

    added the api

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,6 @@ import Hero from './Components/Hero';
 import Menu from './Components/Menu';
 import Layout from './Components/admin/Layout';
 import Dashboard from './pages/admin/Dashboard';
-import { Routes, Route } from 'react-router-dom';
-import Hero from './Components/Hero';
-import Menu from './Components/Menu';
 import FoodDescription from './pages/menu/FoodDescription';
 
 export default function App() {
@@ -13,12 +10,10 @@ export default function App() {
     <Routes>
       <Route path={'/'} element={<Hero />} />
       <Route path={'/menu'} element={<Menu />} />
+      <Route path={'/menu/:id'} element={<FoodDescription />} />
       <Route path={'/admin'} element={<Layout />}>
         <Route path='dashboard' element={<Dashboard />} />
       </Route>
-      <Route path={'/'} element={<Hero />} />
-      <Route path={'/menu'} element={<Menu />} />
-      <Route path={'/menu/:id'} element={<FoodDescription />} />
     </Routes>
   );
 }
--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,11 @@
 // src/axios.js
-import axios from "axios";
+import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: "http://localhost:8000/api", // Replace with your backend URL
+  baseURL: 'http://localhost:8000/api', // Replace with your backend URL
   timeout: 10000, // optional: 10 seconds timeout
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
     // 'Authorization': 'Bearer <token>' // Uncomment if using auth
   },
 });
